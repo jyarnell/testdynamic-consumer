1aa5ba846ea177ba48f0e625b1345ca5df1a5ea8
James Yarnell <jamesy@bajaconsulting.com>
2019-02-23 14:25:30 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
+  state = {
+    Components: undefined
+  };
+  loadRemoteComponent = url => {
+    return new Promise(function(resolve, reject) {
+      var request = new XMLHttpRequest();
+
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          var remoteComponentSrc = request.responseText;
+          // eslint-disable-next-line no-new-func
+          const fn = Function(`return ${remoteComponentSrc}`);
+          // return resolve(fn()(7).default);
+          return resolve(fn());
+        } else {
+          return reject();
+        }
+      };
+
+      request.open("GET", url);
+      request.send();
+    });
+  };
+
+  findComponent = name => {
+    if (!this.state.Components) {
+      return undefined;
+    }
+    let cnt = 1;
+    try {
+      while (true) {
+        const cls = this.state.Components(cnt);
+        if (cls.default && cls.default.name === name) {
+          return cls.default;
+        }
+        cnt++;
+      }
+    } catch {
+      return undefined;
+    }
+  };
+
+  getComponent = async () => {
+    const Components = await this.loadRemoteComponent(
+      "http://localhost:3000/bundle.js"
+    );
+    this.setState({ Components });
+  };
+
+  componentWillMount() {
+    this.getComponent();
+  }
+
+  render = () => {
+    const Comp = this.findComponent("App");
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" id="app">
+        {!Comp && <div>Loading...</div>}
+        {Comp && <Comp />}
       </div>
     );
-  }
+  };
 }
 
 export default App;
